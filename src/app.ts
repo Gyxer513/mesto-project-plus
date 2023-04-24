f1ca1e8b8f7a7c23c8d6fafcfe63155bbd0bc0a0
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-04-24 23:08:21 +0300

    server start

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { NextFunction, Response, Request } from 'express';
+
+const { PORT = 3000 } = process.env;
+
+
+const app = express(); 
+
+app.listen(PORT, () => {
+    // Если всё работает, консоль покажет, какой порт приложение слушает
+    console.log(`App listening on port ${PORT}`)
+}) 
